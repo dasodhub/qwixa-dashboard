c8ee02ae245d61ba678e301280aba12909208dbd
Dasodhub Technologies <online-sales-group@Dasodhub-MacBook-Pro.local>
2026-01-13 16:39:29 -0800

    Install Flowbit Plugin

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include 'layouts/header.php'; ?>
+
+<main>
+<h1>This is the entry point</h1>
+</main>
+
+<?php include 'layouts/footer.php'; ?>
---
+++ layouts/footer.php
@@ -0,0 +1,7 @@
+
+    
+    <script src="./src//index.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@4.0.1/dist/flowbite.min.js"></script>
+
+</body>
+</html>
